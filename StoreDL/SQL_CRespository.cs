53c74359a7489b2e3e9631eb6b84d8af866cce91
Sid Hinson <octobersith@outlook.com>
2022-02-05 14:33:42 -0500

    SQLC_C code updates

--- StoreDL/SQL_CRespository.cs
+++ StoreDL/SQL_CRespository.cs
@@ -14,7 +14,7 @@ namespace StoreDL
             string sqlQuery = @"insert into Customers 
                                 values (@CustomerID, @CFirstName, @CLastName, @CDateofBirth, @CustomerAddress, @CustomerState, @CustomerCity, @CustomerZipCode, @CustCountry, @CustomerEmail, @CPassword)";
 
-            //2.MAP THESE VALUES:
+            //2.MAP THESE VALUES into the sqlQuery string:
             // CustomerID int IDENTITY(1,1) PRIMARY KEY NOT NULL,
             // CFirstName varchar(50),
             // CLastName varchar(50),
@@ -70,7 +70,53 @@ namespace StoreDL
 
         public List<Customers> GetAllCustomers()
         {
-            throw new NotImplementedException();
+            //This operation will be bvery similar to the JSON one, with some caveats
+            //1.CREATE the LIST OF CUSTOMERS
+            List<Customers> listofcutomers = new List<Customers>();
+
+            //2.CREATE the SQL Query string desired
+            string sqlQuery =@"select * from Customers";
+
+            //3.ESTABLISH the CONNECTION
+             using(SqlConnection con = new SqlConnection("Server=tcp:revature-retrodb.database.windows.net,1433;Initial Catalog=RetroBarbarianDB;Persist Security Info=False;User ID=RetroDBAdmin;Password=Skidrow345!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                //3.OPEN the CONNECTION
+                con.Open();
+
+                //4.CREATE the SQL Command
+                SqlCommand command = new SqlCommand(sqlQuery, con);
+                
+                //5.The class used here is SQLReader which is designed to READ the TABLE of whatever command you have executed
+                //C# only understands its classes and class libraries so this was created to do this for C# -> READ SQL statement TABLE OUTPUTS
+                //READER will hold the FORM of the table that contains all the Customer Data
+                //The EXECUTE READER also creates the READER
+                SqlDataReader reader = command.ExecuteReader();
+
+                //6. Here a WHILE LOOP is used to read the ROW
+                //The Reader.READ method returns a bool; FALSE if NO ROW, True if ROW
+                //This way, the WHILE LOOP will keep running until no more rows left in table
+                while(reader.Read())
+                {
+                    //Here WE NEED TO MAP it to a class to get this to work
+                    //Here we have a new list object, and we state each list property
+                    //NOTE : Bases in SQL are ZERO BASED SO MAPPING for both starts at 0 [ ~ i.eCustomerID[0] = Column Customers.CustomerID[0]]
+                    listofcutomers.Add(new Customers(){
+                            CustomerID = reader.GetInt32(1),
+                            CFirstName = reader.GetString(2),
+                            CLastName = reader.GetString(3),
+                            CDateofBirth = reader.GetString(4),
+                            CustomerAddress = reader.GetString(5),
+                            CustomerState = reader.GetString(6),
+                            CustomerCity = reader.GetString(7),
+                            CustomerZipcode = reader.GetString(8),
+                            CustCountry = reader.GetString(9),
+                            CustomerEmail = reader.GetString(10),
+                            CPassword = reader.GetString(11)
+                    });
+
+                }
+            }
+            return listofcutomers;
         }
     }
 }
